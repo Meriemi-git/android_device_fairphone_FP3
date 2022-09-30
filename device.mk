7537df39712c1e8afe350e282ac9d19874aeaef5
tribual <tribual@web.de>
2022-09-30 11:55:36 +0200

    FP3: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- device.mk
+++ device.mk
@@ -150,7 +150,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     AntHalService \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth@1.0.vendor \
     audio.bluetooth.default \
