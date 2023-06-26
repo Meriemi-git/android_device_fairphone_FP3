d02bb8cc74c75a032a684a3f7eea09e8a402f350
Meriemi-git <meriemi@protonmail.com>
2023-06-26 11:57:07 +0200

    Add extra apps

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,10 @@ DEVICE_PATH := device/fairphone/FP3
 # Switch to dedicated msm8953 HAL
 TARGET_ENFORCES_QSSI := true
 
+# Include extras apps for personnal buil : 
+# - Bitwarden
+
+INCLUDES_EXTRA_APPS := true
 
 # A/B updater
 AB_OTA_UPDATER := true
